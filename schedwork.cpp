f81d94ce4dff8c280e91ed70da011d5a05676289
Daniela Banda-Hernandez <bandaher@usc.edu>
2025-12-06 01:29:21 +0000

    wordle under 5 sec and schedwork working

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool backtrack(const AvailabilityMatrix& avail, size_t dailyNeed, 
+            size_t maxShifts, DailySchedule& shed, vector<size_t>& shiftsUsed, 
+            size_t day, size_t slot, size_t k);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +39,48 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
+    
+    size_t n = avail.size();
+    size_t k = avail[0].size();
+    vector<size_t> shiftsUsed(k, 0);
+    sched.assign(n, vector<Worker_T>(dailyNeed, INVALID_ID));
+                                                     // day, slot in day
+    return backtrack(avail, dailyNeed, maxShifts, sched, shiftsUsed, 0, 0, k);
 
 }
 
+bool backtrack(const AvailabilityMatrix& avail, size_t dailyNeed, 
+            size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsUsed, 
+            size_t day, size_t slot, size_t k) 
+{
+
+    // base case: last day AND all slots filled
+    if (day == sched.size()) {
+        return true;
+    }
+
+    // go to nxt day
+    if (slot == dailyNeed) {
+        return backtrack(avail, dailyNeed, maxShifts, sched, shiftsUsed, day + 1, 0, k);
+    }
+
+    // try all workers for this day 
+    for (size_t w = 0; w < k; w++) {
+        if(!avail[day][w]) continue;
+
+        if (shiftsUsed[w] >= maxShifts) continue; 
+
+        // choose worker
+        sched[day][slot] = w;
+
+        shiftsUsed[w]++;
+
+        if (backtrack(avail, dailyNeed, maxShifts, sched, shiftsUsed, day, slot + 1, k)) return true;
+
+        // undo
+        shiftsUsed[w]--;
+        sched[day][slot] = INVALID_ID;
+    }
+
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+// bool hasFloating(const string& word, const string& floating);
+void fillWord (int idx, string curr, const string& in, int floatCount[26], int floatRemaining, const set<string>& dict, set<string>& results);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,55 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> results;
+    int floatCount[26] = {0};
+    for (char c : floating) floatCount[c - 'a']++;
+    string curr = in;
+    fillWord(0, curr, in, floatCount, floating.size(), dict, results);
+    return results; 
 }
 
 // Define any helper functions here
+
+void fillWord (int idx, string curr, const string& in, int floatCount[26], int floatRemaining, const set<string>& dict, set<string>& results) {
+    // base case
+    if (idx == (int)curr.size()) {
+        if (floatRemaining == 0 && dict.find(curr) != dict.end()) results.insert(curr);
+        return;
+    }
+
+    // fixed letter
+    if (in[idx] != '-') {
+        curr[idx] = in[idx];
+        
+        fillWord(idx+1, curr, in, floatCount, floatRemaining, dict, results);
+        return;
+    }
+
+    int blanks = 0;
+    for (int i = idx+1; i < (int)curr.size(); i++) {
+      if (curr[i] == '-') blanks++;
+    }
+
+    int totalSlots = blanks + 1; // include current blank;
+
+    for (char c = 'a'; c <= 'z'; c++) {
+      if (floatRemaining == totalSlots && floatCount[c -'a'] == 0) continue;
+
+        curr[idx] = c;
+
+        int newCount[26];
+
+        for (int i = 0; i < 26; i++) newCount[i] = floatCount[i];
+
+        int newRemain = floatRemaining;
+        if (newCount[c - 'a'] > 0) {
+            newCount[c - 'a']--;
+            newRemain--;
+        }
+        // remaining floating > remaing blanks AFTER placing c
+        if (newRemain <= blanks) {
+          fillWord(idx + 1, curr, in, newCount, newRemain, dict, results);
+        }
+    }
+}
